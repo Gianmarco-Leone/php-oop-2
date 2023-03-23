1a139de2ed523a112c1e9db4dced1a7384a59e43
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-03-23 15:44:32 +0100

    add class Category

--- index.php
+++ index.php
@@ -4,14 +4,32 @@ class Product{
     public $name;
     public $poster;
     public $price;
+    public $category;
 
-    public function __construct($_name, $_poster, $_price)
+    public function __construct(string $_name, string $_poster, int $_price, Category $_category)
     {
         $this->name = $_name;
         $this->poster = $_poster;
         $this->price = $_price;
+        $this->category = $_category;
     }
-}
+};
+
+class Category {
+    public $name;
+
+    public function __construct($_name)
+    {
+        $this->name = $_name;
+    }
+};
+
+$category_1 = new Category("Cane");
+$category_2 = new Category("Gatto");
+
+$product_1 = new Product("Croccantini", "linkfoto", 32, $category_1);
+
+var_dump($product_1);
 
 ?>
 
